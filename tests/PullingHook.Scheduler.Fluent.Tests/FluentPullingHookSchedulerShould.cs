47401727650ff7ad74bf504bfb33964474c9ade4
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-05-17 23:25:38 +0200

    add scheduler tests

---
+++ tests/PullingHook.Scheduler.Fluent.Tests/FluentPullingHookSchedulerShould.cs
@@ -0,0 +1,18 @@
+﻿using NSubstitute;
+using Xunit;
+
+namespace PullingHook.Scheduler.Fluent.Tests
+{
+    public class FluentPullingHookSchedulerShould
+    {
+        [Fact]
+        public void Start()
+        {
+            var manager = Substitute.For<IPullingHookManager<int, int>>();
+            var scheduler = new FluentPullingHookScheduler<int, int>();
+            scheduler.Start(manager);
+
+            manager.ScheduledAction.ReceivedWithAnyArgs();
+        }
+    }
+}
