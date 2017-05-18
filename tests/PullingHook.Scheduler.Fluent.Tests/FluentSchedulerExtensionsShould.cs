4ae301c520972fcb66b9127618e4e6a48e500fa8
Luk Vermeulen <luk.vermeulen@gmail.com>
2017-05-18 22:25:28 +0200

    add FluentSchedulerExtensions tests

---
+++ tests/PullingHook.Scheduler.Fluent.Tests/FluentSchedulerExtensionsShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FluentScheduler;
+using NSubstitute;
+using Xunit;
+
+namespace PullingHook.Scheduler.Fluent.Tests
+{
+    public class FluentSchedulerExtensionsShould
+    {
+        [Theory]
+        [InlineData(3)]
+        [InlineData(300)]
+        [InlineData(3000)]
+        public void ToRunEvery(int seconds)
+        {
+            var schedule = Substitute.For<Schedule>((Action)(() => { }));
+            var interval = TimeSpan.FromSeconds(seconds);
+            schedule.ToRunEvery(interval);
+
+            schedule.Received().ToRunEvery((int)Math.Ceiling(Math.Abs(interval.TotalSeconds))).Seconds();
+        }
+    }
+}
